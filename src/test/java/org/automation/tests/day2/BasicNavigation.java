49cdecc34ab5c8f27ae84856e0949d66aad61301
vvyugovskiy <resifar@gmail.com>
2020-03-01 13:54:06 -0500

    Added test

--- src/test/java/org/automation/tests/day2/BasicNavigation.java
+++ src/test/java/org/automation/tests/day2/BasicNavigation.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // to start selenium script we need :
         // setup webdriver (browser driver) and create webdriver object
@@ -15,6 +15,22 @@ public class BasicNavigation {
         /*RemoteWebDriver*/ ChromeDriver driver = new ChromeDriver();
         // In selenium, everything starts from WebDriver interface
         driver.get("http://google.com");
+
+
+        Thread.sleep(3000); // for demo wait 3 seconds
+
+        // method that returns page title
+        // you can see the name of browser
+        String title = driver.getTitle(); // returns<title> some title</title> text
+        String expectedTitle = "Google";
+        System.out.println("Title is: "+ title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
+        driver.close(); // to close browser
 
 
     }
